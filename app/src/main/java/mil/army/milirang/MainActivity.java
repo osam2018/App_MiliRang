099f3dd231425fd87426d2797bd7d4c121cb15fd
qorcjftns <qorcjftns2@gmail.com>
2018-10-23 13:31:12 +0900

    Layout structure changed

--- app/src/main/java/mil/army/milirang/MainActivity.java
+++ app/src/main/java/mil/army/milirang/MainActivity.java
@@ -1,5 +1,6 @@
 package mil.army.milirang;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import mil.army.milirang.report.ReportCreateActivity;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,17 +23,33 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton report_fab = (FloatingActionButton) findViewById(R.id.report_fab);
+        report_fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, ReportCreateActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
+
+        FloatingActionButton schedule_fab = (FloatingActionButton) findViewById(R.id.schedule_fab);
+        schedule_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, ReportCreateActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        openReportView();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +59,16 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void openReportView() {
+        findViewById(R.id.schedule_view).setVisibility(View.INVISIBLE);
+        findViewById(R.id.report_view).setVisibility(View.VISIBLE);
+    }
+
+    private void openScheduleView() {
+        findViewById(R.id.schedule_view).setVisibility(View.VISIBLE);
+        findViewById(R.id.report_view).setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -80,21 +109,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        /*
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_report_inbox) {
+            openReportView();
+        } else if (id == R.id.nav_schedule_event) {
+            openScheduleView();
         }
-        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/mil/army/milirang/report/ReportCreateActivity.java
@@ -0,0 +1,16 @@
+package mil.army.milirang.report;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import mil.army.milirang.R;
+
+public class ReportCreateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_create);
+    }
+
+}
