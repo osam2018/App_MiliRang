8a73098380cd4c7c0a77f2ef40cb821091097326
qorcjftns <qorcjftns2@gmail.com>
2018-10-24 09:41:51 +0900

    Activity => AppCompatActivity

--- app/src/main/java/mil/army/milirang/report/ReportCreateActivity.java
+++ app/src/main/java/mil/army/milirang/report/ReportCreateActivity.java
@@ -2,10 +2,11 @@ package mil.army.milirang.report;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import mil.army.milirang.R;
 
-public class ReportCreateActivity extends Activity {
+public class ReportCreateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
